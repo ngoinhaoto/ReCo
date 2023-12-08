478ccd6ac70d26709f60fd387343e4582e51a600
Long Vo <116516246+Longcodedao@users.noreply.github.com>
2023-12-08 21:46:59 +0700

    Change

--- LLMWeb/Web_Dialog/utils.py
+++ LLMWeb/Web_Dialog/utils.py
@@ -4,9 +4,7 @@ import io
 from langchain.schema.messages import HumanMessage
 from copy import deepcopy
 import aiohttp
-import torchvision.transforms as transforms
 import os
-import torch
 from collections import OrderedDict
 
 import chainlit as cl
@@ -50,54 +48,3 @@ async def generate_images(prompt, image_model):
             
             return elements
         
-
-
-class Normalize_image(object):
-    """Normalize given tensor into given mean and standard dev
-
-    Args:
-        mean (float): Desired mean to substract from tensors
-        std (float): Desired std to divide from tensors
-    """
-
-    def __init__(self, mean, std):
-        assert isinstance(mean, (float))
-        if isinstance(mean, float):
-            self.mean = mean
-
-        if isinstance(std, float):
-            self.std = std
-
-        self.normalize_1 = transforms.Normalize(self.mean, self.std)
-        self.normalize_3 = transforms.Normalize([self.mean] * 3, [self.std] * 3)
-        self.normalize_18 = transforms.Normalize([self.mean] * 18, [self.std] * 18)
-
-    def __call__(self, image_tensor):
-        if image_tensor.shape[0] == 1:
-            return self.normalize_1(image_tensor)
-
-        elif image_tensor.shape[0] == 3:
-            return self.normalize_3(image_tensor)
-
-        elif image_tensor.shape[0] == 18:
-            return self.normalize_18(image_tensor)
-
-        else:
-            assert "Please set proper channels! Normlization implemented only for 1, 3 and 18"
-
-
-def load_checkpoint_mgpu(model, checkpoint_path):
-    if not os.path.exists(checkpoint_path):
-        print("----No checkpoints at given path----")
-        return 
-    
-    model_state_dict = torch.load(checkpoint_path, map_location=torch.device("cpu"))
-    new_state_dict = OrderedDict()
-    for k, v in model_state_dict.items():
-        name = k[7:]  # remove `module.`
-        new_state_dict[name] = v
-
-    model.load_state_dict(new_state_dict)
-    print("----checkpoints loaded from path: {}----".format(checkpoint_path))
-    
-    return model
